817e01ee6ebad7b5d24bfaf7b51021eef6637792
Carlo <ca.fernando415@gmail.com>
2023-10-27 12:42:56 -0400

    completed package and repo setup

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    tabWidth: 2,
+    trailingComma: 'all',
+    singleAttributePerLine: true,
+    printWidth: 65,
+}
